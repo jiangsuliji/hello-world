04ccf0555100fc51926b07dba698012da58236a4
jiangsuliji <jiangsuliji@gmail.com>
2017-12-27 18:20:52 -0800

    added an exmaple makefile

---
+++ basics/Point.cpp
@@ -0,0 +1,38 @@
+/*
+ *          File: Point.cpp
+ * Last Modified: February 19, 2000
+ *         Topic: Writing Make Files
+ * ----------------------------------------------------------------
+ */
+
+#include "Point.h"
+
+//using namespace std;
+
+Point::Point()
+{
+  x = 0;
+  y = 0;
+}
+
+Point::Point(int xval, int yval)
+{
+  x = xval;
+  y = yval;
+}
+
+void Point::move(int dx, int dy)
+{
+  x += dx;
+  y += dy;
+}
+
+int Point::get_x() const
+{
+  return x;
+}
+
+int Point::get_y() const
+{
+  return y;
+}
